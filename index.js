2f203dc6abeab0b124d15ce5a40d748b276d1e0e
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2023-08-14 15:09:38 -0400

    express server is ready to move forward

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-console.log('Hello, world!');
+const express = require('express');
+const app = express();
 
+// Define routes
+app.get('/health', (req, res) => {
+  res.json({ message: 'OK' });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
